d83b6cefad1835e3f9241758f192836fe15fb748
karthik <reddy.gopher@gmail.com>
2023-05-25 11:16:40 +0530

    Fix issue

--- 572-subtree-of-another-tree.go
+++ 572-subtree-of-another-tree.go
@@ -4,24 +4,22 @@
 
 // A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
 
- 
-
 // Example 1:
-
 
 // Input: root = [3,4,5,1,2], subRoot = [4,1,2]
 // Output: true
 // Example 2:
 
-
 // Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
 // Output: false
-func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
-   if root == nil {
-		return false
-   }
+package main
 
-    sameTree := isSameTree(root, subRoot)
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+	if root == nil {
+		return false
+	}
+
+	sameTree := sameTree(root, subRoot)
 	if sameTree {
 		return true
 	}
@@ -29,15 +27,14 @@ func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
 	return isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
 }
 
-
-func isSameTree(s *TreeNode, t *TreeNode) bool {
+func sameTree(s *TreeNode, t *TreeNode) bool {
 	if s == nil && t == nil {
 		return true
 	}
 
 	if s == nil || t == nil || s.Val != t.Val {
 		return false
-	} 
+	}
 
-	return isSameTree(s.Left, t.Left) && isSameTree(s.Right, t.Right)
+	return sameTree(s.Left, t.Left) && sameTree(s.Right, t.Right)
 }
