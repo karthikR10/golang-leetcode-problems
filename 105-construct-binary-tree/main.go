b384648a1f54fb52f98685f26f8b7e2a2e7ee954
karthik <reddy.gopher@gmail.com>
2023-05-25 20:16:20 +0530

    Add construct binary tree 105

---
+++ 105-construct-binary-tree/main.go
@@ -0,0 +1,66 @@
+// https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+// 105. Construct Binary Tree from Preorder and Inorder Traversal
+// Medium
+// 12.8K
+// 378
+// Companies
+// Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+// Example 1:
+
+// Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+// Output: [3,9,20,null,null,15,7]
+// Example 2:
+
+// Input: preorder = [-1], inorder = [-1]
+// Output: [-1]
+
+// Constraints:
+
+// 1 <= preorder.length <= 3000
+// inorder.length == preorder.length
+// -3000 <= preorder[i], inorder[i] <= 3000
+// preorder and inorder consist of unique values.
+// Each value of inorder also appears in preorder.
+// preorder is guaranteed to be the preorder traversal of the tree.
+// inorder is guaranteed to be the inorder traversal of the tree.
+
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 || len(inorder) == 0 {
+		return nil
+	}
+
+	root := &TreeNode{Val: preorder[0]}
+	mid := indexOf(inorder, root.Val)
+
+	root.Left = buildTree(preorder[1:mid+1], inorder[:mid])
+	root.Right = buildTree(preorder[mid+1:], inorder[mid+1:])
+
+	return root
+}
+
+func indexOf(arr []int, val int) int {
+	for i, v := range arr {
+		if v == val {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	preorder := []int{3, 9, 20, 15, 7}
+	inorder := []int{9, 3, 15, 20, 7}
+
+	fmt.Println(buildTree(preorder, inorder))
+}
